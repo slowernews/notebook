d3c1decc4626e67e1a0c221b7b3f0580e33cdafb
E. Ferreira <johnnyzeze1+gi@gmail.com>
2018-02-10 18:07:27 +0000

    +002

--- chrome-extensions-cheat-sheet.js
+++ chrome-extensions-cheat-sheet.js
@@ -1,1 +1,27 @@
-// how to build a simple chrome extension
+// Each extension has the following files:
+//
+// a) A manifest.json file
+// b) Optional: One or more HTML files (unless the extension is a theme)
+// c) Optional: One or more JavaScript files
+// d) Optional: Any other files your extension needs—for example, image files
+//
+// * Create a simple manifest.json
+// You can already load it on chrome://extensions/ after activating developer mode.
+// Just come back here and reload the page to update the extension.
+
+{
+    "manifest_version": 2,                  //v1 will not work anymore
+    "name": "My Extension",
+    "version": "0.1",
+    "content_scripts": [
+        {
+            "matches": ["<all_urls>"],      // or specific pages: "https://mail.google.com/*"
+            "js": ["extension.js"]
+        }
+    ]
+}
+
+// * Create a extension.js file. E.g.
+// By default, Chrome injects content scripts after the DOM is complete.
+
+alert("Hello from your Chrome extension!")
