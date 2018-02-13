17f88f25e218ae83b03a81381c6ccde8ed239a7d
E. Ferreira <johnnyzeze1+gi@gmail.com>
2018-02-13 16:11:35 +0000

    +004

---
+++ JS-and-Jquery-cheat-sheet.js
@@ -0,0 +1,8 @@
+// JS array methods
+
+array.map()		// functional way to iterate over an array
+array.join()	// join array elements in one string
+
+// How jquery work..
+
+$('one string! describing the elements you want to select').jqueryMethod().pipingMethodsPossible()
