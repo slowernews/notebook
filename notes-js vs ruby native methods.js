85de475c18686a6111c08919ed531933d9dcf452
E. Ferreira <johnnyzeze1+gi@gmail.com>
2019-04-11 15:20:43 +0100

    +110

--- notes-js vs ruby native methods.js
+++ notes-js vs ruby native methods.js
@@ -1,42 +1,46 @@
-// Native methods that return a boolean by inspecting if...
+/* Native methods that return a boolean by inspecting if... */
 
 jsArray.every()                 | .some()               // every|any element pass a test
+ //rubyArray                    | .any? {}
 jsArrayOrString.includes()                              // contains the specified element/chars
- //rubyArrayOrString.include?()
+ //rubyArrayOrString.include?() | .empty?               ##                                     |length = 0
 jsString.startsWith()           | .endsWith()           // begins|ends with specified chars
  //rubyString.start_with?()     | .end_with?()
 Array.isArray(jsDatatype)                               // it's an array
 
-// Native methods to acess and return the index of...
+/* Native methods to acess and return the index of... */
 
 jsArrayOrString.indexOf()       | .lastIndexOf()        // the first|last found occurrence of a specified value
- //rubyString.index()           | .rindex()
+ //rubyArrayOrString.index()    | .rindex()
 jsArray.findIndex()                                     // the first element that pass a test
 jsString.search()                                       // the first ocurrence of a specified value or REGEX
 
-// Native methods to acess and return...
+/* Native methods to acess and return... */
 
 jsString.match()                                        // matches against a REGEX
 jsString.charAt()               | .charCodeAt()         // the char|unicode of the char at a specified index
-jsArrayOrString[]                                       // the char or element at a specified index
- //rubyArrayOrString[]
- //rubyArray.first              | .last                 ## first|last element
+jsArrayOrString[index]                                  // the char or element at a specified index
+ //rubyArrayOrString[index|index,lenght|range]
+ //rubyString[regex]
+ //rubyArray.fetch()									## similar but throws error if index out of bounds
+ //rubyArray.first     | .last  | .sample(n)            ## first|last element|random n element(s)
+ //rubyArray.take(n)            | .drop(n)              ## first n elements | elements but the first n
 jsArray.find()                                          // the first element that passes a test
 
-// Native methods to mutate by...
+/* Native methods to mutate the array by... */
 
-jsArray.shift()                 | .pop()                // removing the first/last element. Returns that element
-jsArray.unshift()               | .push()               // adding new elements to the begin|end. Returns new length
-jsArray.splice()                                        // adding|removing elements
+jsArray.shift()                 | .pop()                // removing first|last element. Returns that element
+ //rubyArray.shift  | .shift(n) | .pop  | .pop(n)       ## removing first|first n|last|last n element(s). Returns them
+jsArray.splice()                                        // adding/removing elements
 jsArray.copyWithin()                                    // copying elements to and from specified positions
 jsArray.fill()                                          // filling from a start index to an end index with a static value
 jsArray.reverse()               | .sort()               // reversing the order|sorting the elements
- //rubyArrayOrString.reverse!
+ //rubyArray.reverse!           | .sort! | .shuffle!
 jsArray.forEach()                                       // calling a function on each element
- //rubyArray.each {}
+ //rubyArray.each {}            | .cycle(n) {}          ##                                   |n times
  //rubyArray.reverse_each {}    | .each_index {}        ## on reverse order|with the index as the argument 
 
-// Native methods to create a new array...
+/* Native methods to create a new array... */
 
 jsArray.filter()                                        // with every element that pass a test
 jsArray.map()                                           // with the result of calling a function on each element
@@ -44,17 +48,17 @@ jsArray.map()                                           // with the result of ca
 jsString.split()                                        // with substrings
 Array.from(jsIterableObject)                            // from an iterable object
 
-// Native methods to return...
+/* Native methods to return... */
 
 jsArray.reduce()                | .reduceRight()        // a single value by reducing (left-right|right-left)
 jsArray.entries()  | .keys()    | .values()             // an iterator with key-value pairs|keys|values for each index
 
-// Native methods to create a new string/array...
+/* Native methods to create a new string/array... */
 
 jsArrayOrString.concat()                                // joining two or more strings/arrays
 jsArrayOrString.slice()                                 // with a selected part
 
-// Native methods to create a new string...
+/* Native methods to create a new string... */
 
 jsString.trim()  | .trimStart() | .trimEnd()                        	// removing whitespace from both ends|left|right
 jsString.replace()                                                  	// with a value replacing another value or REGEX
@@ -62,12 +66,13 @@ jsString.substr()               | .substring()                      	// with cha
 jsString.toLowerCase()          | .toUpperCase()                    	// converted to lowercase|uppercase
  //rubyString.downcase          | .upcase  | .capitalize  | .swapcase
  //rubyString.downcase!         | .upcase! | .capitalize! | .swapcase!  ## mutates string
-jsArray.join()                                                      	// by joining all elements (you can define a separator)
+ //rubyString.reverse!                                                  ## mutates string
+jsArray.join()                                                      	// by joining all elements (can define a separator)
 jsDatatype.toString()                                               	// by convertion and returns the result
 jsDate.toDateString()                                               	// converting the readable part of a date
 JSON.stringify()                | .parse()                          	// from an object|object from a JSON string
 
-// Native jsDate methods to...
+/* Native jsDate methods to... */
 
 jsDate.getFullYear()            | .setFullYear()        // get|set year as a four digit number (yyyy)
 jsDate.getMonth()               | .setMonth()           // get|set month as a number (0-11)
@@ -80,15 +85,15 @@ jsDate.getTime()                | .setTime()            // get|set time (millise
 jsDate.getDay()                                         // get weekday as a number (0-6)
 jsDate.now()                    | .parse()              // return millisecs since 01-01-1970 until now|provided date
 
-// JS regular expressions
-// syntax: /pattern/modifiers; 
+/* JS regular expressions */
+/* syntax: /pattern/modifiers; */
 
 var regex = /w3schools/i;       // e.g. w3schools is a pattern, i is a modifier 
 
 jsRegex.test(string)            // search a string for a pattern and returns true or false
 jsRegex.exec(string)            // search a string for a pattern and returns the found text or null
 
-// regExp patterns to match...
+/* REGEX patterns to match... */
 
 [abc]   [^abc]                  // any of the characters between|not between the brackets range
 [0-9]   [^0-9]                  // any of the digits between|NOT between the brackets range
@@ -114,14 +119,14 @@ n{X,}                           // any string that contains a sequence of at lea
 ^n      n$                      // any string with n at the beginning/end of it
 ?=n     ?!n                     // any string that is followed|not followed by a specific string n
 
-// regExp modifiers to...
+/* REGEX modifiers to... */
 
 i                               // case-insensitive matching
 g                               // global match (all matches rather than stopping after first match)
 m                               // multiline matching
 x                               // ignore whitespace
 
-// JS Math object relevant methods and properties that return...
+/* JS Math object relevant methods and properties that return... */
 
 Math.pow()          | x ** y                  // the value of x to the power of y
 Math.sqrt()         | .cbrt()                 // the square|cubic root of x
@@ -131,8 +136,9 @@ Math.max()          | .min()                  // the number with the max|min val
 Math.floor()        | .ceil()   | round()     // rounds downwards|upwards|to the nearest integer
 Math.trunc()        | .abs()                  // the integer part|the absolute value of a number
 
-// JS properties to...
+/* JS properties to... */
 
 jsArrayOrString.length          // return the number of elements
+ //rubyArray.length or .size 
 jsDatatype.constructor          // return the function that created the datatype object's prototype
 jsDatatype.prototype            // add properties and methods to a datatype
