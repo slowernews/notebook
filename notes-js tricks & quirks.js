edfdeb252622b8aabad60a040fc8037ce8fdb381
E. Ferreira <johnnyzeze1+gi@gmail.com>
2019-09-05 12:45:25 +0100

    +137

--- notes-js tricks & quirks.js
+++ notes-js tricks & quirks.js
@@ -1,9 +1,9 @@
 // QUIRKS
 
 'a' < 3			// False. JS tries to convert 'a' into a number but becomes NaN. Anything compared with NaN is false
-null == undefined	// true
-5 + '5'			// '55'. if either is a string, the other is converted and concatenated
 'Boy' > 'ant'		// False, upper-case letters ASCII value is smaller than on lower-case.
+5 + '5'			// '55'. if either is a string, the other is converted and concatenated
+null == undefined	// true
 Number(null)		// 0
 
 var i = 0x2c		// 44 - starting with '0x' means hex number (2×16 + 12(c) = 44)
@@ -15,24 +15,17 @@ var i = 5e-6		// 0.000005 - "e-notation"
 // LOOPS
 
 do {statement} while (expression)	// it executes at least once
-
 while (expression) {statement}		// it might not execute
 
 for(var i=0; i < 10; i++) {statement}
+for(;;) {statement}					// infinite loop	
+for(var propName in window) {document.write(propName)}	// loop on an object
 
-for(;;) {statement}			// infinite loop	
-
-for(var propName in window) { document.write(propName)}		// loop on an object
-
-switch(animal) {
-	case 'cow': giveHay();
-	break;
-	case 'rabbit': giveCarrots();
-	break;
-	default: rest();
-	break;
+switch(animal){
+	case 'cow': giveHay(); break;
+	case 'rabbit': giveCarrots(); break;
+	default: rest(); break;
 }
-
 
 
 // OTHER
